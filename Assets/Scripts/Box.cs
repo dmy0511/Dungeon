cd017aeb5a433d992d7a67501cdd317a91e92834
dmy0511 <127921600+dmy0511@users.noreply.github.com>
2023-06-27 20:44:24 +0900

    4.1
    스테이지2 완성
    악마 추가, 초록 물약 추가

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -20,6 +20,7 @@ public class Box : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player" && !hasCollided)
         {
+            Time.timeScale = 0.0f;
             bPanel.gameObject.SetActive(true);
             hasCollided = true;
         }
@@ -42,17 +43,20 @@ public class Box : MonoBehaviour
     public void no()
     {
         bPanel.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 
     public void IncreasePlayerSpeed()
     {
         boxPanel.SetActive(false);
         playerMovement.IncreaseSpeed();
+        Time.timeScale = 1.0f;
     }
     
     public void HealHp()
     {
         boxPanel.SetActive(false);
         playerMovement.HealHp();
+        Time.timeScale = 1.0f;
     }
 }
